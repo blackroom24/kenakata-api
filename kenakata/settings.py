a8641848b278203d54c0d9abcdc9b6efee1a8afe
Sanket Debnath <sanketdebnath24@gmail.com>
2021-11-13 20:53:49 +0530

    feat(products): added base model for products

--- kenakata/settings.py
+++ kenakata/settings.py
@@ -18,7 +18,7 @@ environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+BASE_URL = env("BASE_URL")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     "rest_framework.authtoken",
     "corsheaders",
     "djoser",
+    "products",
 ]
 CORS_ALLOWED_ORIGINS = ["https://localhost:8080"]
 MIDDLEWARE = [
@@ -127,6 +128,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = "/static/"
+MEDIA_URL = env("MEDIA_URL")
+MEDIA_ROOT = BASE_DIR / MEDIA_URL
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
---
+++ products/models.py
@@ -0,0 +1,70 @@
+from django.db import models
+from django.core.files import File
+from io import BytesIO
+from PIL import Image
+
+from kenakata.settings import BASE_URL
+
+# Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField()
+
+    class Meta:
+        ordering = ("name",)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return f"/{self.slug}/"
+
+
+class Product(models.Model):
+    category = models.ForeignKey(
+        Category, related_name="products", on_delete=models.CASCADE
+    )
+    name = models.CharField(max_length=255)
+    slug = models.SlugField()
+    description = models.TextField(null=True, blank=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image = models.ImageField(upload_to="uploads/", blank=True, null=True)
+    thumbnail = models.ImageField(upload_to="uploads/", blank=True, null=True)
+    date_added = models.DateField(auto_now_add=True)
+
+    class Meta:
+        ordering = ("-date_added",)  # Order in decending
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return f"/{self.category.slug}/{self.slug}/"
+
+    def get_image(self):
+        if self.image:
+            return BASE_URL + self.image.url
+        else:
+            return ""
+
+    def get_thumbnail(self):
+        if self.thumbnail:
+            return BASE_URL + self.thumbnail.url
+        else:
+            if self.image:
+                self.thumbnail = self.make_thumbnail(self.image)
+                self.save()
+                return BASE_URL + self.thumbnail.url
+            else:
+                return ""
+
+    def make_thumbnail(self, image, size=(300, 200)):
+        img = Image.open(image)
+        img.convert("RGB")
+        img.thumbnail(size)
+        thumb_IO = BytesIO()
+        img.save(thumb_IO, "JPEG", quality=85)
+        thumbnail = File(thumb_IO, name=image.name)
+        return thumbnail

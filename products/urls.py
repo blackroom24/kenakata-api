01ef0ad1b32a603cdc157533467862d36df2141a
Sanket Debnath <sanketdebnath24@gmail.com>
2021-12-03 13:26:41 +0530

    feat: added view for each product

--- products/urls.py
+++ products/urls.py
@@ -4,4 +4,8 @@ from products import views
 urlpatterns = [
     path("product-list/", views.ProductList.as_view()),
     path("products/<slug:category_slug>/", views.CategoryDetail.as_view()),
+    path(
+        "products/<slug:category_slug>/<slug:product_slug>/",
+        views.ProductDetail.as_view(),
+    ),
 ]
--- products/views.py
+++ products/views.py
@@ -1,6 +1,4 @@
 from django.http.response import Http404
-from django.shortcuts import render
-from rest_framework import serializers
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Product, Category
@@ -24,4 +22,19 @@ class CategoryDetail(APIView):
     def get(self, request, category_slug, format=None):
         category = self.get_category(category_slug=category_slug)
         serializer = CategorySerializer(category)
+        return Response(serializer.data)
+
+
+class ProductDetail(APIView):
+    def get_product(self, category_slug, product_slug):
+        try:
+            return Product.objects.filter(category__slug=category_slug).get(
+                slug=product_slug
+            )
+        except Product.DoesNotExist:
+            raise Http404
+
+    def get(self, request, category_slug, product_slug, format=None):
+        product = self.get_product(category_slug, product_slug)
+        serializer = ProductSerializer(product)
         return Response(serializer.data)

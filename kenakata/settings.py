f7d79b10f01d5089771b2a91e3749b54f2227311
Sanket Debnath <sanketdebnath24@gmail.com>
2021-12-03 18:10:52 +0530

    ch: heroku chores

--- kenakata/settings.py
+++ kenakata/settings.py
@@ -16,14 +16,12 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-BASE_URL = "http:localhost:8000"
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "f_)*$6xz#a7k(6ir&u@+tq8h@_t_9%3nr%9g5z4vdp#*a4)a*o"
-STRIPE_SECRET_KEY = os.environ.get("STRIPE_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -47,7 +45,7 @@ INSTALLED_APPS = [
     "products",
     "orders",
 ]
-CORS_ALLOWED_ORIGINS = ["http://localhost:8080"]
+CORS_ORIGIN_ALLOW_ALL = True
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",

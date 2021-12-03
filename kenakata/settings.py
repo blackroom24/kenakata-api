1d1ba74ba4a710c47085dbf7fe8428c6f4910709
Sanket Debnath <sanketdebnath24@gmail.com>
2021-12-03 20:55:17 +0530

    ch: BASE_URL fixed

--- kenakata/settings.py
+++ kenakata/settings.py
@@ -20,7 +20,7 @@ from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+BASE_URL = config("BASE_URL", default="http://localhost:8000")
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 

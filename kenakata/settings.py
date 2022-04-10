67015327ec6cc97de5fe2d286c99e0c94a8c771b
Sanket Debnath <sanketdebnath24@gmail.com>
2022-04-10 11:48:36 +0530

    fix: media storage files and ignoring from source

--- kenakata/settings.py
+++ kenakata/settings.py
@@ -131,8 +131,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = "/static/"
-MEDIA_URL = "/media/"
+STATIC_URL = f"{BASE_DIR}/static/"
+MEDIA_URL = f"{BASE_DIR}/media/"
 MEDIA_ROOT = BASE_DIR / MEDIA_URL
 
 # Default primary key field type
